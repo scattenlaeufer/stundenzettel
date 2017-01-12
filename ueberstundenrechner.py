2e26fb0b8625fe30e4d23c0e9192338b8e5c5909
Björn Guth <bjoern.guth@rwth-aachen.de>
2017-01-12 20:50:54 +0100

    added year to week number and replaced tabs with spaces

--- ueberstundenrechner.py
+++ ueberstundenrechner.py
@@ -11,49 +11,50 @@ calendars = principal.calendars()
 calendar = None
 
 for cal in calendars:
-	if config.calendar in str(cal):
-		calendar = cal
-		break
+    if config.calendar in str(cal):
+        calendar = cal
+        break
 
 events = calendar.events()
 
-#for event in events:
-#	print(event)
-
 def work_duration(event):
-	data = event._data.split('BEGIN:VEVENT')[1].split('END:VEVENT')[0]
-	vevent = data.split('\n')
-	categories = []
-	start_str = ''
-	end_str = ''
-	for line in vevent:
-		if 'CATEGORIES:' in line:
-			categories.append(line.split(':')[1])
-		elif 'DTSTART' in line:
-			start_str = line.split(':')[1]
-		elif 'DTEND' in line:
-			end_str = line.split(':')[1]
-	if config.category in categories:
-		start = datetime.datetime.strptime(start_str, '%Y%m%dT%H%M%S')
-		end = datetime.datetime.strptime(end_str, '%Y%m%dT%H%M%S')
-		return start, end
+    data = event._data.split('BEGIN:VEVENT')[1].split('END:VEVENT')[0]
+    vevent = data.split('\n')
+    categories = []
+    start_str = ''
+    end_str = ''
+    for line in vevent:
+        if 'CATEGORIES:' in line:
+            categories.append(line.split(':')[1])
+        elif 'DTSTART' in line:
+            start_str = line.split(':')[1]
+        elif 'DTEND' in line:
+            end_str = line.split(':')[1]
+    if config.category in categories:
+        start = datetime.datetime.strptime(start_str, '%Y%m%dT%H%M%S')
+        end = datetime.datetime.strptime(end_str, '%Y%m%dT%H%M%S')
+        return start, end
 
 hours = {}
 
 for event in events:
-	work_time = work_duration(event)
-	if work_time:
-		work_dur = work_time[1] - work_time[0]
-		week =  int(work_time[0].strftime('%W'))
-		if week in hours.keys():
-			hours[week] += work_dur
-		else:
-			hours[week] = work_dur
+    work_time = work_duration(event)
+    if work_time:
+        work_dur = work_time[1] - work_time[0]
+        week =  work_time[0].strftime('%Y-%W')
+        if week in hours.keys():
+            hours[week] += work_dur
+        else:
+            hours[week] = work_dur
 
 total_overtime = 0
 
-for week, dur in hours.iteritems():
-	overtime = dur.total_seconds()/3600-19
-	total_overtime += overtime
-	print(week, overtime)
+weeks = hours.keys()
+weeks.sort()
+#for week, dur in hours.iteritems():
+for week in weeks:
+    dur = hours[week]
+    overtime = dur.total_seconds()/3600-19
+    total_overtime += overtime
+    print(week, overtime)
 print(total_overtime)
